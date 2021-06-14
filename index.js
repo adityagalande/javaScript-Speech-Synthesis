4dd5f2285774e507dae82f72273f3dd5f0a5fa97
Aditya Galande <adityagalande14@gmail.com>
2021-06-15 00:41:57 +0530

    final

--- index.js
+++ index.js
@@ -19,12 +19,33 @@ const stopButton = document.querySelector('#stop');
 
 msg.text = document.querySelector('[name="text"]').value;
 
+
 function populateVoices() {
   voices = this.getVoices();
   const voiceOptions = voices.map(voice => `<option value="${voice.name}">${voice.name}(${voice.lang})</option>`).join('');
   voiceDropdown.innerHTML = voiceOptions;
 }
 
-// speechSynthesis.speak(new SpeechSynthesisUtterance("Hello, this is your browser speaking."));
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if(startOver){
+  speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
-// console.log(this.getVoices());
+voiceDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', toggle.bind(null, false));
